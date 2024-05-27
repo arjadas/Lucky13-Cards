0797012eaed07d9d654af2e6e8699133e8c6d519
arjadas <arjadas17@gmail.com>
2024-05-27 18:31:23 +1000

    Yiyan updated the score calculation logic

--- app/src/main/java/Calc13.java
+++ app/src/main/java/Calc13.java
@@ -5,16 +5,8 @@ public class Calc13 {
 
     private static final int THIRTEEN_GOAL = 13;
 
-    public boolean isThirteenCards(Card card1, Card card2) {
-        return getCardScore(card1) + getCardScore(card2) == THIRTEEN_GOAL;
-    }
-
-    public boolean isThirteenCards(Card card1, Card card2, Card card3) {
-        return getCardScore(card1) + getCardScore(card2) + getCardScore(card3) == THIRTEEN_GOAL;
-    }
-
-    public boolean isThirteenCards(Card card1, Card card2, Card card3, Card card4) {
-        return getCardScore(card1) + getCardScore(card2) + getCardScore(card3) + getCardScore(card4) == THIRTEEN_GOAL;
+    public boolean isThirteenCards(Card... cards) {
+        return isThirteenFromPossibleValues(cards);
     }
 
     public boolean isThirteenMixedCards(List<Card> privateCards, List<Card> publicCards) {
@@ -45,12 +37,33 @@ public class Calc13 {
         if (isThirteenCards(privateCards.get(0), privateCards.get(1), publicCards.get(0), publicCards.get(1))) {
             return true;
         }
-
         return false;
     }
+    // Checks sum of the possible values of the given cards = 13.
+    // handles cards with multiple possible values（ace 0，1）
+    // use recursive  to check all combinations of the possible values.
+    //if any combination of the possible values of the cards equals 13, false otherwise.
+    private boolean isThirteenFromPossibleValues(Card... cards) {
+        int[][] possibleValues = new int[cards.length][];
+        for (int i = 0; i < cards.length; i++) {
+            possibleValues[i] = getPossibleValues(cards[i]);
+        }
+        return checkCombinations(possibleValues, 0, 0);
+    }
 
-    private int getCardScore(Card card) {
+    private boolean checkCombinations(int[][] possibleValues, int index, int currentSum) {
+        if (index == possibleValues.length) {
+            return currentSum == THIRTEEN_GOAL;
+        }
+        for (int value : possibleValues[index]) {
+            if (checkCombinations(possibleValues, index + 1, currentSum + value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private int[] getPossibleValues(Card card) {
         Rank rank = (Rank) card.getRank();
-        return rank.getScoreCardValue();
+        return rank.getPossibleSumValues();
     }
 }
